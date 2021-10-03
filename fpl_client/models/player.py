32204139a727ab113071633d2c7b294d16e1058e
hiracky16 <h.piiice16@gmail.com>
2021-10-03 12:27:35 +0900

    add players photo url

--- fpl_client/models/player.py
+++ fpl_client/models/player.py
@@ -10,6 +10,7 @@ class Player:
         ]
         for a in attributes:
             exec(f"self.{a} = obj['{a}']")
+        self.image_url = f'https://resources.premierleague.com/premierleague/photos/players/250x250/p{obj["photo"]}'
 
     def full_name(self):
         return f"{self.first_name} {self.second_name}"
